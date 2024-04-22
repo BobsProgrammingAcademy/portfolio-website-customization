6ca83c09b2b354448de2ab7e3b3d99e2bf350e21
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2024-04-22 05:59:24 +0200

    Code refactoring

--- frontend/src/components/Technologies.js
+++ frontend/src/components/Technologies.js
@@ -49,18 +49,18 @@ const Technologies = () => {
               align='center'
               fontWeight={700}
               marginTop={theme.spacing(1)}
-              data-aos='fade-up'
               gutterBottom
+              data-aos='fade-up'
             >
               Technologies
             </Typography>
             <Typography
               variant='h6'
               align='center'
-              color={theme.palette.text.secondary}
-              data-aos='fade-up'
+              color={theme.palette.text.secondary}              
               marginTop={4}
               marginBottom={6}
+              data-aos='fade-up'
             >
               Technologies we use to build our products
             </Typography>
@@ -70,11 +70,7 @@ const Technologies = () => {
               <Grid item xs={12} md={3} key={i}>
                 <Box
                   width={1}
-                  height={1}
-                  data-aos='fade-up'
-                  data-aos-delay={100}
-                  data-aos-offset={100}
-                  data-aos-duration={600}
+                  height={1}                  
                   component={Card}
                   display='flex'
                   flexDirection='column'
@@ -82,6 +78,10 @@ const Technologies = () => {
                   boxShadow={0}
                   variant='outlined'
                   borderRadius={2}
+                  data-aos='fade-up'
+                  data-aos-delay={100}
+                  data-aos-offset={100}
+                  data-aos-duration={600}
                 >
                   <CardContent
                     sx={{
